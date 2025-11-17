bfa870901d48e99653cb814af95e7d8842e6a71e
nycki <nupanick@gmail.com>
2025-11-16 20:53:46 -0800

    uninvent wheel

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,18 +3,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setInputDirectory('content');
     eleventyConfig.addPassthroughCopy({ 'static': '/' });
 
+    eleventyConfig.addCollection('$galleryTags', (collectionsApi) => {
+        const tags = new Set();
+        for (const page of collectionsApi.getFilteredByTag('$gallery')) {
+            for (const tag of page.data.tags) {
+                if (tag == '$gallery') continue;
+                tags.add(tag);
+            }
+        }
+        return Array.from(tags).toSorted();
+    });
+
     // use template for all pages unless otherwise stated!
     eleventyConfig.addGlobalData('layout', 'base.njk');
-};
-
-// get all tags inside a named collection, not counting the name of the collection itself.
-module.exports.getCollectionTags = (filterTag) => (_, fullData) => {
-    const tags = new Set();
-    for (const page of fullData.collections[filterTag]) {
-        for (const tag of page.data.tags) {
-            if (tag == filterTag) continue;
-            tags.add(tag);
-        }
-    }
-    return Array.from(tags).toSorted();
 };
